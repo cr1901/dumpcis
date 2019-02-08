b32d04b604ea44075f13d37989076e67cd57e1e9
William D. Jones <thor0505@comcast.net>
2019-02-07 23:39:44 -0500

    Add skeleton for code which actually reads CIS.

--- dumpcis.c
+++ dumpcis.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+#include <dos.h>
+
 #include "pcmctrl.h"
 
-uint8_t __far * PCM_WIN = (uint8_t __far *) 0xe0000000;
+uint8_t __far * PCM_WIN = (uint8_t __far *) MK_FP(0xe000, 0000);
+#define PCM_LINEAR 0xe0000uL
 
 int main()
 {
@@ -25,9 +29,13 @@ int main()
 
         for(socket = 0; socket < 2; socket++)
         {
-            uint8_t isr = pcm_read(pcm, socket, 0x01);
-            uint8_t power = 0;
+            /* Preserve as much previous state as we can, be as minimally
+            invasive as possible. */
+            uint8_t prev_power = 0;
+            uint8_t prev_adrwin = 0;
+            uint8_t count = 0;
 
+            uint8_t isr = pcm_read(pcm, socket, 0x01);
             if((isr & 0xC))
             {
                 printf("Found a card in Socket %d.\n", socket);
@@ -37,8 +45,28 @@ int main()
                 continue;
             }
 
-            power = pcm_read(pcm, socket, 0x02);
-            printf("Power: %X\n", power);
+            /* Power socket on. */
+            prev_power = pcm_read(pcm, socket, 0x02);
+            pcm_write(pcm, socket, 0x02, prev_power | (1 << 4));
+            while((pcm_read(pcm, socket, 0x01) & 0x40) == 0);
+
+            prev_adrwin = pcm_read(pcm, socket, 0x06);
+            pcm_write(pcm, socket, 0x06, 0); /* Disable all windows so we can
+                                                get our own window to read CIS. */
+
+
+            /* Enable attribute memory and read CIS. */
+            //pcm_write(pcm, socket, 0x06, 0x40);
+
+            for(count = 0; count < 16; count++)
+            {
+                uint8_t dat = PCM_WIN[count];
+                printf("%X ", dat);
+            }
+
+            /* Restore modified registers. */
+            pcm_write(pcm, socket, 0x06, prev_adrwin);
+            pcm_write(pcm, socket, 0x02, prev_power);
         }
     }
 
