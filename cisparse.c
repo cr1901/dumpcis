15ca89c7744d77a501a4b11bc0ac2f92e2959c3d
William D. Jones <thor0505@comcast.net>
2019-02-12 18:33:28 -0500

    Fix uninitialized read from array, improve parser loop.

--- cisparse.c
+++ cisparse.c
@@ -7,17 +7,17 @@ static uint16_t inline read_entry(char buf[], uint8_t __far * start, bool use_16
 
 int cis_parse(cis_parser_t * state, uint8_t __far * start_addr)
 {
-    char buf[257];
-    uint8_t count, count2;
+    char buf[257] = { '\0' };
+    uint8_t byte_count, tup_count;
     uint16_t len;
 
-    for(count2 = 0; !(buf[0] == 0x14 || buf[0] == 0xFF); count2++)
+    for(tup_count = 0; (!(buf[0] == 0x14 || buf[0] == 0xFF) && tup_count < 255); tup_count++)
     {
         len = read_entry(buf, start_addr, true);
 
-        for(count = 0; count < len; count++)
+        for(byte_count = 0; byte_count < len; byte_count++)
         {
-            printf("%X ", buf[count]);
+            printf("%X ", buf[byte_count]);
         }
 
         start_addr += 2*len; /* TODO: Support 8-bit meaningfully. */
